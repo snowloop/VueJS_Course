6a5711800312b9f200141bdf68232a4dd115b857
= <=>
2019-11-10 15:26:13 +0100

    Second lesson over

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#app',
   data : {
     name : "Alexandre",
-    job : "Web developper"
+    job : "Web developper",
+    website : "https://www.linkedin.com/in/alexandrejosse/",
+    websiteTag : '<a href="https://www.linkedin.com/in/alexandrejosse/" target="_blank">My website bis</a>'
   },
   methods : {
     greet:time =>{
