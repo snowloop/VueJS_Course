34990bb96d68217dd9a8b5769eb2cb44a6ed14c5
= <=>
2019-11-10 16:11:26 +0100

    Lesson 3 over

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 new Vue({
   el: '#app',
-  data : {
-    name : "Alexandre",
-    job : "Web developper",
-    website : "https://www.linkedin.com/in/alexandrejosse/",
-    websiteTag : '<a href="https://www.linkedin.com/in/alexandrejosse/" target="_blank">My website bis</a>'
+  data: {
+    age: 25,
+    x:1,
+    y:1,
   },
-  methods : {
-    greet:time =>{
-      return "Good " + time + this.name
+  methods: {
+    updateXY :function(event){
+      this.x = event.offsetX
+      this.y = event.offsetY
+      this.age++
     }
   }
 })
