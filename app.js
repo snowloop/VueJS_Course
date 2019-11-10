79d838b336ba0df9976541818beebb15ac36bd7a
AlexaJosse <=>
2019-11-11 00:05:00 +0100

    Lesson 8 over

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 new Vue({
   el: '#app',
   data: {
-available :true,
-empty :true
+    health: 100,
+    damage: 0,
+    destroyed : false
   },
-  methods: {},
-  computed :{
-    compClass:function(){
-      return {
-        empty : this.empty
+  methods: {
+    punch: function() {
+      if (this.damage< 90) {
+        this.damage +=10;
+      } else {
+        this.damage +=10;
+        this.destroyed = true
       }
+    },
+    restart:function(){
+      this.destroyed = false;
+      this.damage = 0;
+    }
+  },
+  computed: {
+    getHealth: function() {
+      return this.health - this.damage
     }
   }
 })
