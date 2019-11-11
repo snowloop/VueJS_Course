04aeb73e7d20a576de1b7c81d29255274fd0934d
AlexaJosse <=>
2019-11-11 16:04:03 +0100

    Lesson 10 over

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
-Vue.component('hellocomp', {
-  template: '<p>Hello my name is {{ name }}.<button v-on:click="changeName">Change my name</button></p>',
-  data: function() {
-    return {
-      name: "Yoshi"
-    }
+new Vue({
+  el: '#app-one',
+  data :{
+    output : "It's empty"
   },
   methods: {
-    changeName: function() {
-      this.name = "Mario"
+    readRefs : function(){
+      console.log(this.$refs)
+      this.output = this.$refs.input.value
     }
-  }
-
-})
-
-var appOne = new Vue({
-  el: '#app-one',
-  methods: {},
-  computed: {}
-})
-
-
-var appTwo = new Vue({
-  el: '#app-two',
-  methods: {},
+  },
   computed: {}
 })
